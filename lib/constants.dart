17aca2f30d4aaf66a7c5b6bae5eb02e4f6a2e699
Mohammad Shamim Hossain <58461912+DeveloperShamim@users.noreply.github.com>
2021-07-19 11:21:40 +0600

    Toggle between Signin and Register Compleate

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Colors.blue;
+const kPrimaryColor = Color(0xFFC657FF);
+
+const kPrimaryLightColor = Color(0xFF1E7783);
+
+var boxDecoration = BoxDecoration(
+  gradient: LinearGradient(
+      begin: Alignment.bottomCenter,
+      end: Alignment.topCenter,
+      colors: [
+        Color(0xFF1D7D87),
+        Color(0xFF305678),
+        Color(0xFF4E4879),
+        Color(0xFF5F467D),
+        Color(0xFF674B7C),
+      ]),
+);
+var appBardeco = BoxDecoration(
+  gradient: LinearGradient(
+      begin: Alignment.bottomRight,
+      end: Alignment.topLeft,
+      colors: [
+        Colors.purple,
+        Colors.red,
+      ]),
+);
